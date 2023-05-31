1b78763f7408190fd7949b7f56744906800352cf
AndrewMatsiash <rabotatnadomne@gmail.com>
2023-05-31 15:10:31 +0300

    add adaptive header for screen 768px

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const popup = document.querySelector('.popup')
 const overlay = document.querySelector('.overlay')
+const burgerButton = document.querySelector('.burger-menu-button')
+const menu = document.querySelector('.header__main-nav')
 
 function showPopup() {
 	popup.style.display = 'block'
@@ -13,9 +15,15 @@ function hidePopup() {
 	document.body.style.overflow = 'auto'
 }
 
+function toggleMenu() {
+	burgerButton.classList.toggle('open')
+	menu.classList.toggle('open')
+}
+
 document
 	.querySelector('.open-popup-button')
 	.addEventListener('click', showPopup)
 document
 	.querySelector('.close-popup-button')
 	.addEventListener('click', hidePopup)
+burgerButton.addEventListener('click', toggleMenu)
