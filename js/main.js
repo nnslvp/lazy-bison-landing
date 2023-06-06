046705a11c32efc523ae0b347c3f5de6a8c61a00
AndrewMatsiash <rabotatnadomne@gmail.com>
2023-06-06 22:30:27 +0300

    feat:add adaptive section cases  for screen 375px

--- js/main.js
+++ js/main.js
@@ -20,11 +20,58 @@ function toggleMenu() {
 	menu.classList.toggle('open')
 }
 
-document
-	.querySelector('.open-popup-button')
-	.addEventListener('click', showPopup)
+// document
+// 	.querySelector('.open-popup-button')
+// 	.addEventListener('click', showPopup)
 
-document
-	.querySelector('.close-popup-button')
-	.addEventListener('click', hidePopup)
-burgerButton.addEventListener('click', toggleMenu)
+// document
+// 	.querySelector('.close-popup-button')
+// 	.addEventListener('click', hidePopup)
+// burgerButton.addEventListener('click', toggleMenu)
+
+const slider = document.querySelector('.slider-container')
+
+// let mySwiper = new Swiper(slider, {
+// 	slidesPerView: 3,
+// 	spaceBetween: 10,
+// 	loop: true,
+// 	pagination: {
+// 		el: '.swiper-pagination',
+// 		clickable: true,
+// 	},
+// 	navigation: {
+// 		nextEl: '.swiper-button-next',
+// 		prevEl: '.swiper-button-prev',
+// 	},
+// })
+
+let mySwiper
+
+function mobileSlider() {
+	if (window.innerWidth <= 600 && slider.dataset.mobile == 'false') {
+		mySwiper = new Swiper(slider, {
+			slidesPerView: 1,
+			spaceBetween: 0,
+			loop: true,
+			slideClass: 'swiper-slide',
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+		})
+		slider.dataset.mobile = 'true'
+	}
+
+	if (window.innerWidth > 600) {
+		slider.dataset.mobile = 'false'
+		if (slider.classList.contains('swiper-container-initialized')) {
+			mySwiper.destroy()
+		}
+	}
+}
+
+mobileSlider()
+
+window.addEventListener('resize', () => {
+	mobileSlider()
+})
