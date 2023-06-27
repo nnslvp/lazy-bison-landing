c71b84a76262e184bbe5694375fba5131f456e90
AndrewMatsiash <rabotatnadomne@gmail.com>
2023-06-27 22:32:25 +0300

    feat:add validate form and show errors input

--- js/animateWorkers.js
+++ js/animateWorkers.js
@@ -1,23 +1,23 @@
-// const workers = document.querySelector('.workers')
-// const widthScreen = document.documentElement.clientWidth
+const workers = document.querySelector('.workers')
+const widthScreen = document.documentElement.clientWidth
 
-// function shuffleArray(array) {
-// 	for (let i = array.length - 1; i >= 0; i--) {
-// 		const j = Math.floor(Math.random() * (i + 1))
-// 		;[array[i], array[j]] = [array[j], array[i]]
-// 	}
-// 	return array
-// }
+function shuffleArray(array) {
+	for (let i = array.length - 1; i >= 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1))
+		;[array[i], array[j]] = [array[j], array[i]]
+	}
+	return array
+}
 
-// if (widthScreen > 375) {
-// 	setInterval(() => {
-// 		const items = Array.from(workers.children)
-// 		const shuffledItems = shuffleArray(items)
+if (widthScreen > 375) {
+	setInterval(() => {
+		const items = Array.from(workers.children)
+		const shuffledItems = shuffleArray(items)
 
-// 		workers.innerHTML = ''
+		workers.innerHTML = ''
 
-// 		shuffledItems.forEach(item => {
-// 			workers.appendChild(item)
-// 		})
-// 	}, 3000)
-// }
+		shuffledItems.forEach(item => {
+			workers.appendChild(item)
+		})
+	}, 3000)
+}
