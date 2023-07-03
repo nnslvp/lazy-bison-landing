9f2e86b726134d0cf13a40139589739bce6ff3fd
AndrewMatsiash <rabotatnadomne@gmail.com>
2023-07-03 15:15:27 +0300

    feat:add animation open and close burger menu

--- js/animateWorkers.js
+++ js/animateWorkers.js
@@ -1,29 +1,29 @@
-// const workers = document.querySelector('.workers')
-// const widthScreen = document.documentElement.clientWidth
+const workers = document.querySelector('.workers')
+const widthScreen = document.documentElement.clientWidth
 
-// function shuffleArray(array) {
-// 	for (let i = array.length - 1; i >= 0; i--) {
-// 		const j = Math.floor(Math.random() * (i + 1))
-// 		;[array[i], array[j]] = [array[j], array[i]]
-// 	}
-// 	return array
-// }
+function shuffleArray(array) {
+	for (let i = array.length - 1; i >= 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1))
+		;[array[i], array[j]] = [array[j], array[i]]
+	}
+	return array
+}
 
-// if (widthScreen > 375) {
-// 	setInterval(() => {
-// 		const items = Array.from(workers.children)
-// 		const shuffledItems = shuffleArray(items)
+if (widthScreen > 375) {
+	setInterval(() => {
+		const items = Array.from(workers.children)
+		const shuffledItems = shuffleArray(items)
 
-// 		workers.classList.add('fade-out')
+		workers.classList.add('fade-out')
 
-// 		setTimeout(() => {
-// 			workers.innerHTML = ''
+		setTimeout(() => {
+			workers.innerHTML = ''
 
-// 			shuffledItems.forEach(item => {
-// 				workers.appendChild(item)
-// 			})
+			shuffledItems.forEach(item => {
+				workers.appendChild(item)
+			})
 
-// 			workers.classList.remove('fade-out')
-// 		}, 250)
-// 	}, 3000)
-// }
+			workers.classList.remove('fade-out')
+		}, 250)
+	}, 3000)
+}
