3a9c767eeaefeaf486746030dcc6e1b68b5d44f5
AndrewMatsiash <rabotatnadomne@gmail.com>
2023-07-10 22:36:59 +0300

    feat:add projectsSlider use swiper then windowWIth more 425 px and slides > 1

--- js/sliders.js
+++ js/sliders.js
@@ -1,29 +1,78 @@
 const sliders = document.querySelectorAll('.swiper')
-let mySwipers = []
+const [projectSlider, workersSlider] = sliders
+const projectSwiperWrapper = projectSlider.querySelector('.swiper-wrapper')
+const projectSwiperWrapperContent = projectSwiperWrapper.innerHTML
 
-function mobileSlider(slider) {
-	if (window.innerWidth <= 425 && slider.dataset.mobile === 'false') {
-		const newSwiper = new Swiper(slider, {
-			slidesPerView: 1,
-			spaceBetween: 0,
-			loop: true,
-			init: true,
-			pagination: {
-				el: slider.querySelector('.swiper-pagination'),
-				clickable: true,
-			},
-		})
-		mySwipers.push(newSwiper)
-		slider.dataset.mobile = 'true'
-	} else {
-		slider.dataset.mobile = 'false'
-		mySwipers.forEach(swiper => swiper.destroy())
-	}
+console.log(projectSwiperWrapperContent)
+
+const projectSliderSwiper = new Swiper(projectSlider, {
+	slidesPerView: 1,
+	spaceBetween: 0,
+	loop: true,
+	init: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+})
+
+function initializeWorkersSlider() {
+	const workersSwiper = new Swiper(workersSlider, {
+		slidesPerView: 1,
+		spaceBetween: 0,
+		loop: true,
+		init: true,
+		pagination: {
+			el: workersSlider.querySelector('.swiper-pagination'),
+			clickable: true,
+		},
+	})
 }
 
-sliders.forEach(slider => {
-	window.addEventListener('resize', () => {
-		mobileSlider(slider)
-	})
-	mobileSlider(slider)
-})
+function mobileSlider() {
+	if (window.innerWidth <= 425 && projectSlider.dataset.mobile === 'true') {
+		const slides = projectSlider.querySelectorAll('.project-slide__item')
+
+		slides.forEach(el => {
+			el.classList.remove(...el.classList)
+			el.classList.add('swiper-slide')
+		})
+
+		projectSwiperWrapper.innerHTML = ''
+
+		projectSliderSwiper.appendSlide(slides)
+	} else if (
+		window.innerWidth > 425 &&
+		projectSlider.dataset.mobile === 'false'
+	) {
+		projectSwiperWrapper.insertAdjacentHTML(
+			'beforeend',
+			projectSwiperWrapperContent
+		)
+	}
+	// if (window.innerWidth <= 425 && workersSlider.dataset.mobile === 'false') {
+	// 	const newWorkersSwiper = new Swiper(workersSlider, {
+	// 		slidesPerView: 1,
+	// 		spaceBetween: 0,
+	// 		loop: true,
+	// 		init: true,
+	// 		pagination: {
+	// 			el: workersSlider.querySelector('.swiper-pagination'),
+	// 			clickable: true,
+	// 		},
+	// 	})
+
+	// 	projectSlider.dataset.mobile = 'true'
+	// 	workersSlider.dataset.mobile = 'true'
+	// } else {
+	// 	projectSlider.dataset.mobile = 'false'
+	// 	workersSlider.dataset.mobile = 'false'
+	// 	if (window.innerWidth >= 425 && workersSlider.dataset.mobile === 'true') {
+	// 		workersSlider.destroy()
+	// 	}
+	// }
+}
+
+window.addEventListener('resize', mobileSlider)
+
+mobileSlider()
