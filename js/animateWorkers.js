199fd8795c61dfb7e337dd08d078eb10581d5325
AndrewMatsiash <rabotatnadomne@gmail.com>
2023-08-14 18:53:07 +0300

    feat:fix logo,Bozon rename Bison,fix width  vertical line explore section,fix animation smoothness workers,add create  min.css file

--- js/animateWorkers.js
+++ js/animateWorkers.js
@@ -2,28 +2,30 @@ const workers = document.querySelector('.workers__list')
 const widthScreen = document.documentElement.clientWidth
 
 function shuffleArray(array) {
-	for (let i = array.length - 1; i >= 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1))
-		;[array[i], array[j]] = [array[j], array[i]]
-	}
-	return array
+    for (let i = array.length - 1; i >= 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
 if (widthScreen > 425) {
-	setInterval(() => {
-		const items = Array.from(workers.children)
-		const shuffledItems = shuffleArray(items)
+    setInterval(() => {
+        const items = Array.from(workers.children);
+        const shuffledItems = shuffleArray(items);
 
-		workers.classList.add('fade-out')
+        workers.classList.add('fade-out');
 
-		setTimeout(() => {
-			workers.innerHTML = ''
+        setTimeout(() => {
+            while (workers.firstChild) {
+                workers.removeChild(workers.firstChild);
+            }
 
-			shuffledItems.forEach(item => {
-				workers.appendChild(item)
-			})
+            shuffledItems.forEach(item => {
+                workers.appendChild(item);
+            });
 
-			workers.classList.remove('fade-out')
-		}, 250)
-	}, 3000)
+            workers.classList.remove('fade-out');
+        }, 250);
+    }, 3000);
 }
