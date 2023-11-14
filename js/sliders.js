56a999bd4a706ef31c1162caa3429ff9f78c202b
AndrewMatsiash <rabotatnadomne@gmail.com>
2023-11-14 14:25:26 +0300

    feat:add section blog  for screens 1440px-768px-425px

--- js/sliders.js
+++ js/sliders.js
@@ -1,31 +1,63 @@
 const sliders = document.querySelectorAll(".swiper");
-const [projectSlider, workersSlider] = sliders;
+const [projectSlider, blogSlider, workersSlider] = sliders;
 const projectSwiperWrapper = projectSlider.querySelector(".swiper-wrapper");
 const projectSwiperWrapperContent = projectSwiperWrapper.innerHTML;
 const projectsSlides = projectSlider.querySelectorAll(".swiper-cases-slide");
 let newWorkersSwiper;
 let newProjectSwiper;
+let newBlogSlider;
 
 newProjectSwiper = new Swiper(projectSlider, {
-  slidesPerView: "auto",
+  slidesPerView: 1,
   spaceBetween: 8,
   loop: true,
   init: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: projectSlider.querySelector(".swiper-pagination"),
     clickable: true,
   },
+
   breakpoints: {
     425: {
       slidesPerView: "auto",
       spaceBetween: 8,
     },
+
     768: {
       slidesPerView: "auto",
       spaceBetween: 10,
     },
+
     1440: {
       slidesPerView: 2,
+      spaceBetween: 28,
+    },
+  },
+});
+
+newBlogSlider = new Swiper(blogSlider, {
+  slidesPerView: "auto",
+  spaceBetween: 8,
+  loop: true,
+  init: true,
+  pagination: {
+    el: blogSlider.querySelector(".swiper-pagination"),
+    clickable: true,
+  },
+
+  breakpoints: {
+    425: {
+      slidesPerView: "auto",
+      spaceBetween: 8,
+    },
+
+    768: {
+      slidesPerView: "auto",
+      spaceBetween: 16,
+    },
+
+    1440: {
+      slidesPerView: 3,
       spaceBetween: 28,
     },
   },
@@ -44,34 +76,34 @@ function initializeWorkersSlider() {
   });
 }
 
-// function mobileSlider() {
-//   const isMobile = window.innerWidth <= 425;
+function mobileSlider() {
+  const isMobile = window.innerWidth <= 425;
 
-//   if (isMobile && projectSlider.dataset.mobile === "false") {
-//     projectsSlides.forEach((el) => {
-//       el.classList.remove(...el.classList);
-//       el.classList.add("swiper-slide", "case-slide");
-//     });
-//     projectSwiperWrapper.innerHTML = "";
-//     newProjectSwiper.appendSlide(projectsSlides);
-//     projectSlider.dataset.mobile = "true";
-//   }
+  if (isMobile && projectSlider.dataset.mobile === "false") {
+    projectsSlides.forEach((el) => {
+      el.classList.remove(...el.classList);
+      el.classList.add("swiper-slide", "case-slide");
+    });
+    projectSwiperWrapper.innerHTML = "";
+    newProjectSwiper.appendSlide(projectsSlides);
+    projectSlider.dataset.mobile = "true";
+  }
 
-//   if (!isMobile && projectSlider.dataset.mobile === "true") {
-//     projectSwiperWrapper.innerHTML = projectSwiperWrapperContent;
-//     projectSlider.dataset.mobile = "false";
-//   }
+  if (!isMobile && projectSlider.dataset.mobile === "true") {
+    projectSwiperWrapper.innerHTML = projectSwiperWrapperContent;
+    projectSlider.dataset.mobile = "false";
+  }
 
-//   if (isMobile && workersSlider.dataset.mobile === "false") {
-//     initializeWorkersSlider();
-//     workersSlider.dataset.mobile = "true";
-//   }
+  if (isMobile && workersSlider.dataset.mobile === "false") {
+    initializeWorkersSlider();
+    workersSlider.dataset.mobile = "true";
+  }
 
-//   if (!isMobile && workersSlider.dataset.mobile === "true") {
-//     newWorkersSwiper.destroy();
-//     workersSlider.dataset.mobile = "false";
-//   }
-// }
+  if (!isMobile && workersSlider.dataset.mobile === "true") {
+    newWorkersSwiper.destroy();
+    workersSlider.dataset.mobile = "false";
+  }
+}
 
 window.addEventListener("resize", mobileSlider);
 
