d3bf7aa9462cc930adb261db5647c132ff606677
andrei <andrei@andreis-MacBook-Air.local>
2023-11-23 14:03:23 +0300

    feat:add section gallery

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
-import './animateLines.js'
-import './animateWorkers.js'
-import './burgerMenu.js'
-import './dropdown.js'
-import './getConsultationForm.js'
-import './popups.js'
-import './sliders.js'
+// import './animateLines.js'
+// import './animateWorkers.js'
+// import './burgerMenu.js'
+// import './dropdown.js'
+// import './getConsultationForm.js'
+// import './popups.js'
+// import './sliders.js'
+
+const sliderGallery = document.querySelector('.swiper')
+console.log(sliderGallery)
+let newWorkersSwiper
+let newProjectSwiper
+let newBlogSlider
+
+newProjectSwiper = new Swiper(sliderGallery, {
+	slidesPerView: 1,
+	spaceBetween: 10,
+	loop: true,
+	init: true,
+	pagination: {
+		el: sliderGallery.querySelector('.swiper-pagination'),
+		clickable: true,
+	},
+	navigation: {
+		nextEl: document.querySelector('.swiper-btn-next'),
+		prevEl: document.querySelector('.swiper-btn-prev'),
+	},
+})
