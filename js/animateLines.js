9eaf8f82164b9ed3cb5df5579796155e6673dd71
andrei <andrei@andreis-MacBook-Air.local>
2023-11-27 19:09:39 +0300

    fix: fix layout bugs home page

--- js/animateLines.js
+++ js/animateLines.js
@@ -1,153 +1,155 @@
-const grayLines = document.querySelectorAll('.gray-line')
-const verticalGrayLines = document.querySelectorAll('.vertical-gray-line ')
-const widthScreen = window.innerWidth
+export const animateLines = () => {
+	const grayLines = document.querySelectorAll('.gray-line')
+	const verticalGrayLines = document.querySelectorAll('.vertical-gray-line ')
+	const widthScreen = window.innerWidth
 
-const testimonialsSectionVerticalLines = document.querySelectorAll(
-	'.testimonials .vertical-gray-line'
-)
-const exploreSectionVerticalLines = document.querySelectorAll(
-	'.explore .vertical-gray-line'
-)
-const rootFontSize = parseFloat(
-	getComputedStyle(document.documentElement).fontSize
-)
-
-function pxToRem(pxValue) {
+	const testimonialsSectionVerticalLines = document.querySelectorAll(
+		'.testimonials .vertical-gray-line'
+	)
+	const exploreSectionVerticalLines = document.querySelectorAll(
+		'.explore .vertical-gray-line'
+	)
 	const rootFontSize = parseFloat(
 		getComputedStyle(document.documentElement).fontSize
 	)
-	const remValue = pxValue / rootFontSize
-	return remValue + 'rem'
-}
 
-const animationDuration = 1000
-
-const animateLines = lines => {
-	lines.forEach(line => {
-		animateLineHeight(line)
-	})
-}
-
-const animateVerticalLines = line => {
-	if (line.classList.contains('testimonials__gray-line')) {
-		animateLines(testimonialsSectionVerticalLines)
+	function pxToRem(pxValue) {
+		const rootFontSize = parseFloat(
+			getComputedStyle(document.documentElement).fontSize
+		)
+		const remValue = pxValue / rootFontSize
+		return remValue + 'rem'
 	}
 
-	if (line.classList.contains('explore__gray-line')) {
-		animateLines(exploreSectionVerticalLines)
-	}
-}
+	const animationDuration = 1000
 
-const getInitialWidth = line => {
-	const computedStyle = getComputedStyle(line)
-	return computedStyle.width
-}
-
-const getInitialHeight = line => {
-	const computedStyle = getComputedStyle(line)
-	return computedStyle.height
-}
-
-const animateLineWith = line => {
-	const width = getInitialWidth(line)
-	const targetWidth = parseFloat(width) / rootFontSize
-	const animationStep = (targetWidth / animationDuration) * 10
-	let currentWidth = 0
-	line.style.opacity = 1
-	const animateWidth = () => {
-		if (currentWidth >= targetWidth) {
-			line.style.maxWidth = 100 + '%'
-			animateVerticalLines(line)
-			return
-		}
-		currentWidth += animationStep
-		line.style.maxWidth = currentWidth + 'rem'
-		requestAnimationFrame(animateWidth)
+	const animateLines = lines => {
+		lines.forEach(line => {
+			animateLineHeight(line)
+		})
 	}
 
-	animateWidth()
-}
-
-const animateLineHeight = line => {
-	const height = getInitialHeight(line)
-	const targetHeight = parseFloat(height) / rootFontSize
-	const animationStep = (targetHeight / animationDuration) * 10
-
-	let currentHeight = 0
-	line.style.opacity = 1
-
-	const animateHeight = () => {
-		if (currentHeight >= targetHeight) {
-			line.style.maxHeight = height
-			animateVerticalLines(line)
-			return
+	const animateVerticalLines = line => {
+		if (line.classList.contains('testimonials__gray-line')) {
+			animateLines(testimonialsSectionVerticalLines)
 		}
 
-		currentHeight += animationStep
-		line.style.maxHeight = currentHeight + 'rem'
-
-		if (currentHeight < targetHeight) {
-			requestAnimationFrame(animateHeight)
+		if (line.classList.contains('explore__gray-line')) {
+			animateLines(exploreSectionVerticalLines)
 		}
 	}
 
-	animateHeight()
-}
+	const getInitialWidth = line => {
+		const computedStyle = getComputedStyle(line)
+		return computedStyle.width
+	}
 
-function elementInViewport(el) {
-	let bounds = el.getBoundingClientRect()
-	return (
-		bounds.top >= 0 &&
-		bounds.top < window.innerHeight &&
-		!el.classList.contains('is-visible')
+	const getInitialHeight = line => {
+		const computedStyle = getComputedStyle(line)
+		return computedStyle.height
+	}
+
+	const animateLineWith = line => {
+		const width = getInitialWidth(line)
+		const targetWidth = parseFloat(width) / rootFontSize
+		const animationStep = (targetWidth / animationDuration) * 10
+		let currentWidth = 0
+		line.style.opacity = 1
+		const animateWidth = () => {
+			if (currentWidth >= targetWidth) {
+				line.style.maxWidth = 100 + '%'
+				animateVerticalLines(line)
+				return
+			}
+			currentWidth += animationStep
+			line.style.maxWidth = currentWidth + 'rem'
+			requestAnimationFrame(animateWidth)
+		}
+
+		animateWidth()
+	}
+
+	const animateLineHeight = line => {
+		const height = getInitialHeight(line)
+		const targetHeight = parseFloat(height) / rootFontSize
+		const animationStep = (targetHeight / animationDuration) * 10
+
+		let currentHeight = 0
+		line.style.opacity = 1
+
+		const animateHeight = () => {
+			if (currentHeight >= targetHeight) {
+				line.style.maxHeight = height
+				animateVerticalLines(line)
+				return
+			}
+
+			currentHeight += animationStep
+			line.style.maxHeight = currentHeight + 'rem'
+
+			if (currentHeight < targetHeight) {
+				requestAnimationFrame(animateHeight)
+			}
+		}
+
+		animateHeight()
+	}
+
+	function elementInViewport(el) {
+		let bounds = el.getBoundingClientRect()
+		return (
+			bounds.top >= 0 &&
+			bounds.top < window.innerHeight &&
+			!el.classList.contains('is-visible')
+		)
+	}
+
+	const sections = document.querySelectorAll(
+		'.explore, .services, .testimonials, .cases'
 	)
-}
 
-const sections = document.querySelectorAll(
-	'.explore, .services, .testimonials, .cases'
-)
+	function handleScroll() {
+		sections.forEach(function (block) {
+			if (elementInViewport(block)) {
+				block.classList.add('is-visible')
+				const lines = document.querySelectorAll(
+					`.${block.className.split(' ')[0]} .gray-line`
+				)
+				lines.forEach(line => {
+					animateLineWith(line)
+				})
+			}
+		})
+	}
 
-function handleScroll() {
-	sections.forEach(function (block) {
-		if (elementInViewport(block)) {
-			block.classList.add('is-visible')
-			const lines = document.querySelectorAll(
-				`.${block.className.split(' ')[0]} .gray-line`
-			)
-			lines.forEach(line => {
-				animateLineWith(line)
+	const line = document.querySelector('.lazy-bison .gray-line')
+
+	animateLineWith(line)
+
+	if (widthScreen > 425) {
+		window.addEventListener('scroll', handleScroll)
+	}
+
+	function handleResize() {
+		const widthScreen = window.innerWidth
+
+		if (widthScreen <= 768) {
+			grayLines.forEach(line => {
+				line.style = null
 			})
+			verticalGrayLines.forEach(line => (line.style = null))
 		}
-	})
-}
 
-const line = document.querySelector('.lazy-bison .gray-line')
-
-animateLineWith(line)
-
-if (widthScreen > 425) {
-	window.addEventListener('scroll', handleScroll)
-}
-
-function handleResize() {
-	const widthScreen = window.innerWidth
-
-	if (widthScreen <= 768) {
-		grayLines.forEach(line => {
-			line.style = null
-		})
-		verticalGrayLines.forEach(line => (line.style = null))
+		if (widthScreen <= 425) {
+			grayLines.forEach(line => {
+				line.style.opacity = 1
+			})
+			verticalGrayLines.forEach(line => (line.style.opacity = 0))
+		} else {
+			grayLines.forEach(line => (line.style.opacity = 1))
+			verticalGrayLines.forEach(line => (line.style.opacity = 1))
+		}
 	}
 
-	if (widthScreen <= 425) {
-		grayLines.forEach(line => {
-			line.style.opacity = 1
-		})
-		verticalGrayLines.forEach(line => (line.style.opacity = 0))
-	} else {
-		grayLines.forEach(line => (line.style.opacity = 1))
-		verticalGrayLines.forEach(line => (line.style.opacity = 1))
-	}
+	window.addEventListener('resize', handleResize)
 }
-
-window.addEventListener('resize', handleResize)
--- js/dropdown.js
+++ js/dropdown.js
@@ -1,60 +1,65 @@
-const buttons = document.querySelectorAll('.services-info__type-button')
-const descriptionsContent = document.querySelectorAll('.descriptions__content')
-const descriptionsText = document.querySelectorAll(
-	'.services-info__description-text'
-)
-const title = document.querySelector('.services-info__title')
-const widthScreen = document.documentElement.clientWidth
+export const dropdown = () => {
+	const buttons = document.querySelectorAll('.services-info__type-button')
+	const descriptionsContent = document.querySelectorAll(
+		'.descriptions__content'
+	)
+	const descriptionsText = document.querySelectorAll(
+		'.services-info__description-text'
+	)
+	const title = document.querySelector('.services-info__title')
+	const widthScreen = document.documentElement.clientWidth
 
-const addDescriptionsTextForSmallScreens = () => {
-	buttons.forEach((button, index) => {
-		const descriptionsTextElement = descriptionsText[index]
-		const descriptionsTextClone = descriptionsTextElement.cloneNode(true)
-		button.insertAdjacentElement('afterend', descriptionsTextClone)
-	})
-}
+	const addDescriptionsTextForSmallScreens = () => {
+		buttons.forEach((button, index) => {
+			const descriptionsTextElement = descriptionsText[index]
+			const descriptionsTextClone = descriptionsTextElement.cloneNode(true)
+			button.insertAdjacentElement('afterend', descriptionsTextClone)
+		})
+	}
 
-buttons.forEach(function (button, index) {
-	button.addEventListener('click', event => {
-		if (widthScreen <= 425) {
-			descriptionsText.forEach(e => {
-				e.classList.remove('slide-in-height')
-			})
+	const handlerClickButtonsDropdownOnSmallScreen = index => {
+		const descriptionTextElements = Array.from(
+			document.querySelectorAll('.services-info__description-text')
+		).filter(element => {
+			return (
+				window.getComputedStyle(element).getPropertyValue('display') !== 'none'
+			)
+		})
+		descriptionTextElements.forEach(e => {
+			e.classList.remove('slide-in-height')
+		})
+		descriptionTextElements[index].classList.add('slide-in-height')
+		return
+	}
 
+	buttons.forEach(function (button, index) {
+		button.addEventListener('click', event => {
+			handlerClickButtonsDropdownOnSmallScreen(index)
 			buttons.forEach(button => {
 				button.classList.remove('active')
 			})
+
+			descriptionsContent.forEach(function (content) {
+				content.classList.remove('show')
+			})
 			event.currentTarget.classList.add('active')
-			descriptionsText[index].classList.add('slide-in-height')
-			return
-		}
-
-		buttons.forEach(button => {
-			button.classList.remove('active')
+			descriptionsContent[index].classList.add('show')
 		})
-
-		descriptionsContent.forEach(function (content) {
-			content.classList.remove('show')
-		})
-		event.currentTarget.classList.add('active')
-
-		descriptionsContent[index].classList.add('show')
 	})
-})
 
-let hasExecuted = false
+	let hasExecuted = false
 
-// Первоначальная проверка при загрузке страницы
-if (widthScreen <= 425) {
-	addDescriptionsTextForSmallScreens()
-	hasExecuted = true
-}
-
-window.addEventListener('resize', e => {
-	const widthScreen = window.innerWidth
-
-	if (widthScreen <= 425 && !hasExecuted) {
+	if (widthScreen <= 425) {
 		addDescriptionsTextForSmallScreens()
 		hasExecuted = true
 	}
-})
+
+	window.addEventListener('resize', e => {
+		const widthScreen = window.innerWidth
+
+		if (widthScreen <= 425 && !hasExecuted) {
+			addDescriptionsTextForSmallScreens()
+			hasExecuted = true
+		}
+	})
+}
